d5093d99b8644c393ffefbcd9d5ce47e891d66a4
Exspa <exspakz@gmail.com>
2023-10-12 16:35:38 +0600

    Add user and chat models
    - Introduced `BaseUser` model extending Django's AbstractUser with required first_name field.
    - Added `UserProfile` model for storing Telegram specific fields: chat_id and token.
    - Created `Message` model with text and timestamp to handle chat messages, limited message text to 1000 characters.

--- factory/accounts/models.py
+++ factory/accounts/models.py
@@ -1,1 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class BaseUser(AbstractUser):
+    REQUIRED_FIELDS = ['first_name']
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return f'{self._meta.verbose_name}: {self.username}'
+
+
+class UserProfile(models.Model):
+    telegram_chat_id = models.CharField(
+        'telegram chat id', max_length=255, blank=True, null=True
+    )
+    telegram_token = models.CharField(
+        'telegram token', max_length=255, unique=True, blank=True, null=True
+    )
+
+    user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'user profile'
+        verbose_name_plural = 'profiles users'
+
+    def __str__(self):
+        return f'{self.user.username}'
--- factory/chat/models.py
+++ factory/chat/models.py
@@ -1,3 +1,8 @@
+from django.core.validators import MaxLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    text = models.TextField('text',  validators=[MaxLengthValidator(1000)])
+    created_at = models.DateTimeField('date created', auto_now_add=True)
+
